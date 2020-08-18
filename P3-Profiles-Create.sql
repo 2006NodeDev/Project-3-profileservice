ff405324efd8ef6151b29bb7588826a9a96b14e7
RchlRiedel <rchlriedel27@gmail.com>
2020-08-18 11:42:28 -0400

    not sure what changed, but this is still just SQL

--- P3-Profiles-Create.sql
+++ P3-Profiles-Create.sql
@@ -2,18 +2,19 @@ create schema project_3_profile_service;
 set schema 'project_3_profile_service';
 
 create table profiles (
-	"user_id" int --need to check if this is really and int and how to relate it (fk, etc.)
-	"batch_id" int --check
-	"nickname" text 
-	"pronouns" text 
-	"hobbies" text
-	"fav_foods" text 
-	"special_trait" text unique
-	"degree" text not null
-	"fav_language" text
-	"relevant_skills" text 
-	"extrovert" boolean 
-	"study_group" boolean 
+	"auth0_user_id" text primary key,
+	"caliber_user_id" text not null unique,
+	"batch_id" text not null, 
+	"nickname" text, 
+	"pronouns" text, 
+	"hobbies" text,
+	"fav_foods" text, 
+	"special_trait" text unique,
+	"degree" text, 
+	"fav_language" text,
+	"relevant_skills" text, 
+	"introvert" boolean, 
+	"study_group" boolean
 );
 
 --https://medium.com/@pandit.summit/kubernetes-helm-initdb-as-a-job-633f0d1261be
