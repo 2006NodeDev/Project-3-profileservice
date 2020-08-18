38bc2ad9edc8e36248f01e9f58f7f5bc209105dc
RchlRiedel <rchlriedel27@gmail.com>
2020-08-18 14:17:15 -0400

    get all endpoints

---
+++ src/utils/profile-dto-to-profile-converter.ts
@@ -0,0 +1,24 @@
+import { ProfileDTO } from "../dtos/profile-dto";
+import { Profile } from "../models/Profile";
+
+export function profileDTOtoProfileConverter(dto:ProfileDTO):Profile{
+
+    return {
+        auth0Id: dto.auth0_user_id,
+        caliberId: dto.caliber_user_id,
+        batchId: dto.batch_id,
+        nickname: dto.nickname,
+        pronouns: dto.pronouns,
+        hobbies: dto.hobbies,
+        favFoods: dto.fav_foods,
+        specialTrait: dto.special_trait,
+        degree: dto.degree,
+        favLangauge: dto.fav_language,
+        relevantSkills: dto.relevant_skills,
+        introvert: dto.introvert,
+        studyGroup: dto.study_group
+    }
+
+
+
+}
