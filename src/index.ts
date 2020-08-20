6cf899bd292cc232d150be8e8d8835a890d3b1ec
RchlRiedel <rchlriedel27@gmail.com>
2020-08-20 13:04:38 -0400

    added health check

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import { profileRouter } from "./routers/profile-router"
 import { corsFilter } from "./middleware/cors-filter"
 
@@ -11,15 +11,20 @@ app.use(corsFilter)
 
 app.use("/profiles", profileRouter)
 
-// app.use((err, req, res, next) => {  
-//     if (err.statusCode) { 
-//         console.log(err);
-//         res.status(err.statusCode).send(err.message)
-//     } else { //if it wasn't one of our custom errors, send generic response
-//         console.log(err); 
-//         res.status(500).send("Oops, something went wrong")
-//     }
-// })
+//health check! for load balancer and build
+app.get('/health', (req: Request, res: Response) => {
+    res.sendStatus(200)
+})
+
+app.use((err, req, res, next) => {  
+    if (err.statusCode) { 
+        console.log(err);
+        res.status(err.statusCode).send(err.message)
+    } else { //if it wasn't one of our custom errors, send generic response
+        console.log(err); 
+        res.status(500).send("Oops, something went wrong")
+    }
+})
 
 //what port do we want?
 app.listen(2007, () => { //start server on port 2007
