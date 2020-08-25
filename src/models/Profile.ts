24eadbc214d06f8d057df3fbf6402f15ebe6369f
Jaboa <jaboahowell@gmail.com>
2020-08-25 17:09:30 -0600

    testing build

--- src/models/Profile.ts
+++ src/models/Profile.ts
@@ -1,17 +1,19 @@
 export class Profile {
-    auth0Id:string;
-    firstName:string;
-    lastName:string;
-    email: string;
-    batchId: string;
-    nickname: string;
-    pronouns: string;
-    hobbies: string;
-    favFoods: string;
-    specialTrait: string;
-    degree: string;
-    favLangauge: string;
-    relevantSkills: string;
-    introvert: Boolean;
-    studyGroup: Boolean;
+  auth0Id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  batchId: string;
+  nickname: string;
+  pronouns: string;
+  hobbies: string;
+  favFoods: string;
+  specialTrait: string;
+  degree: string;
+  favLangauge: string;
+  relevantSkills: string;
+  introvert: Boolean;
+  studyGroup: Boolean;
 }
+
+//testing
