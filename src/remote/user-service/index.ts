eb2daba9cb52a2d07610ef8d3aaf57d5df479720
RchlRiedel <66640876+RchlRiedel@users.noreply.github.com>
2020-08-26 13:23:04 -0400

    fixed the base path url

--- src/remote/user-service/index.ts
+++ src/remote/user-service/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //an optional env for host address or localhost default
-let baseURL = "http://localhost:2006/user-service"; // || process.env['LB_USER_SERVICE_HOST']
+let baseURL = process.env['USER_SERVICE_HOST'] || "http://localhost:2006"; // || 
 
 export const userServiceBaseClient = axios.create({
   baseURL,
